c9bba1e7a1c2f48b19664b569e326f8f4f1cccbe
tomasouza <tassfone@gmail.com>
2022-11-26 11:42:46 -0300

    fix: comment

--- Tipos e Declarações/01_declararVariavel.go
+++ Tipos e Declarações/01_declararVariavel.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var a int = 10 // formato de declaracao completa de uma variavel
 	var b = 20     // o tipo nao e necessario, pois o tipo padrao para a literal numerica 20 = int
-	var c int      // o tipo especificado, mas o valor nao, inicializando o z com valor zero (0)
+	var c int      // o tipo especificado, mas o valor nao, inicializando o c com valor zero (0)
 
 	fmt.Println(a, b, c)
 
