2c72367f76ab825ebdf0e6ed8bd61d294e242ac9
tomasouza <tassfone@gmail.com>
2023-03-03 13:14:24 -0300

    fix: adding comment

--- 04_Estruturas_de_Controle/11_Switch.go
+++ 04_Estruturas_de_Controle/11_Switch.go
@@ -3,18 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	//Estamos procurando por uma palavra de tamanho 5 neste caso
 	words := []string{"a", "bola", "peixe", "abacate",
-		"tomate", "carne"}
+		"eletrecista", "carne"}
 	for _, word := range words {
 		switch size := len(word); size {
 		case 1, 2, 3, 4:
-			fmt.Println(word, "Pequena palavra!")
+			fmt.Println(word, "é uma palavra pequena!")
 		case 5:
 			wordLen := len(word)
-			fmt.Println(word, "Palavra com tamanho exato:", wordLen)
+			fmt.Println(word, "é uma palavra com tamanho exato:", wordLen)
 		case 6, 7, 8, 9:
 		default:
-			fmt.Println(word, "Palavra longa!")
+			fmt.Println(word, "é uma palavra longa!")
 		}
 	}
 }
